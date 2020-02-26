36e4b6ba89540b4081e717a45285893d1bfd9638
dylfaust <mdfaust@ucsc.edu>
2020-02-26 01:11:50 -0800

    Push website initial

---
+++ html/js/drag-and-drop.js
@@ -0,0 +1,76 @@
+var currWindow;
+
+makeWindowDraggable("window-notes");
+makeWindowDraggable("window-console");
+
+function makeWindowDraggable(id)
+{
+  var draggable = document.getElementById(id);
+  draggable.addEventListener("mousedown", function(event){ mouseDown(event, draggable) });
+  draggable.addEventListener("mouseup", function(event){ mouseUp(event) });
+}
+
+function mouseDown(event, window)
+{
+  var canDrag = false;
+  if (window.matches("draggable"))
+  {
+    canDrag = true;
+  }
+  else
+  {
+    var child = window.getElementsByClassName("draggable")[0];
+    if (child)
+    {
+      canDrag = true;
+
+      var posX = event.clientX;
+      var posY = event.clientY;
+
+      var top =  child.getBoundingClientRect().top;
+      var bottom =  child.getBoundingClientRect().bottom;
+
+      console.log(posX + " " + posY + " | " + top + " " + bottom);
+      var withinY = posY > top && posY < bottom;
+
+      canDrag = withinY;
+    }
+  }
+
+
+  if (canDrag)
+  {
+    currWindow = window;
+    currWindow.shiftX = event.clientX - currWindow.getBoundingClientRect().left;
+    currWindow.shiftY = event.clientY - currWindow.getBoundingClientRect().top;
+    currWindow.style.position = 'absolute';
+    currWindow.style.zIndex = 1000;
+    document.body.append(currWindow);
+
+    moveAt(event.pageX, event.pageY);
+    // move the draggable on mousemove
+    document.addEventListener('mousemove', onMouseMove, event);
+  }
+}
+
+function mouseUp(event)
+{
+  document.removeEventListener('mousemove', onMouseMove);
+  currWindow = null;
+}
+
+
+
+  // moves the draggable at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) 
+  {
+    currWindow.style.left = pageX - currWindow.shiftX + 'px';
+    currWindow.style.top = pageY - currWindow.shiftY + 'px';
+  }
+
+  function onMouseMove(event) 
+  {
+    moveAt(event.pageX, event.pageY);
+    console.log("swag");
+  }
