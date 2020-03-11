a1891e54e7e88c83d876f098b6dec467206a31b8
dylfaust <dylfaust@gmail.com>
2020-03-11 00:16:47 -0800

    Fix some styling, make close functional, etc.

---
+++ js/jquery-funcs.js
@@ -0,0 +1,11 @@
+$(function ()
+  {
+    $(".window").each(function ()
+  {
+    var myDiv = $(this);
+    myDiv.resizable();
+  });
+
+    // // Dynamically load pages
+    // $('#portfolio-info-pane').load("html/thelastofus2_small.html");
+  });
