0d28cb3d500a4632d16333c6ebb129bcd0dd10c3
dylfaust <dylfaust@gmail.com>
2020-04-09 03:49:39 -0800

    Update aim

--- js/lottie-test.js
+++ js/lottie-test.js
@@ -3,9 +3,9 @@ var button = document.getElementsByClassName("back-button")[0];
 let backAnimController = bodymovin.loadAnimation({
     container: document.getElementsByClassName("lottie-anim")[0],
     renderer: 'svg',
-    loop: false,
+    loop: true,
     autoplay: false,
     path: "../anims/logo-anim.json"
 });
 
-backAnimController.playSegments([0, 1000], true);
+backAnimController.playSegments([0, 2*30+2], true);
