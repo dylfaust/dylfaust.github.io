9d065120fc0e60781edd488d803c793851f8be8d
dylfaust <dylfaust@gmail.com>
2020-04-18 02:48:55 -0800

    No longer a hack but waters

--- js/logo-anim.js
+++ js/logo-anim.js
@@ -194,26 +194,7 @@ function playOutro()
 // Labeled hack as we really shouldn't need this dupe function
 function isMobileHACK()
 {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  // Windows Phone must come first because its UA also contains "Android"
-  if (/windows phone/i.test(userAgent))
-  {
-    return true;
-  }
-
-  if (/android/i.test(userAgent))
-  {
-    return true;
-  }
-
-  // iOS detection from: http://stackoverflow.com/a/9039885/177710
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)
-  {
-    return true;
-  }
-
-  return false;
+   return $(window).width() <= 600;
 }
 
 function logoOutroFrame(event, forceStates = false)
